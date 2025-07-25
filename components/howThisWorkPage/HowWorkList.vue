e12c0f74f02e6a7abaa99d4e0349006dbae74f27
MrPuta <jakub.pluta02@googlemail.com>
2025-07-25 14:19:02 +0200

    Dodanie responsywnosci

--- components/howThisWorkPage/HowWorkList.vue
+++ components/howThisWorkPage/HowWorkList.vue
@@ -1,16 +1,16 @@
 <template>
-  <Card class="flex items-center w-[50vw] py-[2vh]">
+  <Card class="flex items-center py-[2vh]">
     <template #title>
-      <h1 class="flex justify-center text-4xl font-bold py-[3vh] my-[3vh] bg-[#D74141] rounded-xl text-white">
+      <h1 class="flex justify-center text-4xl font-bold py-[3vh] my-[3vh] bg-[#D74141] rounded-xl text-white text-center">
         {{ $t('pages.howThisWork.title') }}
       </h1>
     </template>
     <template #content>
       <div class="flex flex-col gap-4 text-lg">
-        <h1>
+        <h1 class="font-semibold">
           {{ $t('pages.howThisWork.content.useApp') }}
         </h1>
-        <ol class="pl-[1vw]">
+        <ol class="px-[3vw] md:px-[1vw]">
           <li
             v-for="(step, index) in stepsList"
             :key="index"
@@ -18,7 +18,7 @@
             {{ (index + 1) + '. ' + step }}
           </li>
         </ol>
-        <h1>
+        <h1 class="font-semibold">
           {{ $t('pages.howThisWork.content.procesIsSafe') }}
         </h1>
       </div>
