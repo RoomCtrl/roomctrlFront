47e3e7f88b24a6d0ffccf4b8a2a0bc16823dc26a
MrPuta <jakub.pluta02@googlemail.com>
2025-10-29 09:27:59 +0100

    Poprawa wizualna

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="class flex flex-col justify-center items-center gap-20 h-[100vh]">
+  <div class="flex flex-col justify-center items-center gap-20 h-[100vh]">
     <div class="flex flex-col gap-4 text-center">
       <h1 class="font-extrabold text-7xl">
         Witaj w systemie!
@@ -8,59 +8,47 @@
         Wybierz panel, do którego chcesz przejść
       </h2>
     </div>
-
-    <div class="flex flex-row justify-center items-center gap-8">
+    <div class="flex flex-col xl:flex-row justify-center items-center gap-8">
       <div class="relative">
         <div class="absolute inset-0 rounded-lg bg-red-500/30 blur-2xl opacity-70 group-hover:opacity-100 scale-110 group-hover:scale-125 transition-all duration-700 animate-pulse-slow pointer-events-none" />
-
         <div
-          class="flex flex-col justify-center items-center w-[40rem] h-[20rem] border-8 rounded-lg gap-4 relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-2xl group bg-red-950/70 backdrop-blur-sm"
+          class="flex flex-col justify-center items-center w-[40rem] h-[20rem] border-8 rounded-lg gap-6 relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-2xl group bg-red-950/70 backdrop-blur-sm overflow-hidden"
           :class="adminBorderClass"
           @click="selectRole('admin')"
         >
-          <i
-            class="pi pi-hammer text-red-400 group-hover:scale-110 transition-transform duration-300"
-            style="font-size: 5rem"
+          <img
+            src="/logos/logo_admin_dark_mode.svg"
+            class="h-20 group-hover:scale-110 transition-transform duration-300"
           />
-          <div class="flex flex-row font-bold text-5xl text-white drop-shadow-lg">
-            <h1 class="text-red-400 glow-text-red">
-              A
-            </h1>
-            <h1>
-              dmin
-            </h1>
+
+          <div class="relative z-10 flex flex-col items-center gap-2">
+            <p class="text-lg text-gray-200 text-center px-8">
+              Panel administratora - pełny dostęp do zarządzania systemem i użytkownikami
+            </p>
           </div>
-          <h2 class="text-xl">
-            Pełny dostęp do zarządzania systemem i użytkownikami
-          </h2>
         </div>
       </div>
 
-      <div class="w-[2px] h-[36rem] bg-gradient-to-b from-transparent via-gray-400 to-transparent" />
+      <div class="hidden xl:block w-[2px] h-[36rem] bg-gradient-to-b from-transparent via-gray-400 to-transparent" />
+      <div class="block xl:hidden h-[2px] w-[36rem] bg-gradient-to-r from-transparent via-gray-400 to-transparent" />
 
       <div class="relative">
         <div class="absolute inset-0 rounded-lg bg-green-500/30 blur-2xl opacity-70 group-hover:opacity-100 scale-110 group-hover:scale-125 transition-all duration-700 animate-pulse-slow pointer-events-none" />
-
         <div
-          class="flex flex-col justify-center items-center w-[40rem] h-[20rem] border-8 rounded-lg gap-4 relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-2xl group bg-green-950/70 backdrop-blur-sm"
+          class="flex flex-col justify-center items-center w-[40rem] h-[20rem] border-8 rounded-lg gap-6 relative cursor-pointer transition-all duration-300 hover:scale-105 hover:shadow-2xl group bg-green-950/70 backdrop-blur-sm overflow-hidden"
           :class="userBorderClass"
           @click="selectRole('user')"
         >
-          <i
-            class="pi pi-user text-green-400 group-hover:scale-110 transition-transform duration-300"
-            style="font-size: 5rem"
+          <img
+            src="/logos/logo_dark_mode.svg"
+            class="h-20 group-hover:scale-110 transition-transform duration-300"
           />
-          <div class="flex flex-row font-bold text-5xl text-white drop-shadow-lg">
-            <h1 class="text-green-400 glow-text-green">
-              U
-            </h1>
-            <h1>
-              żytkownik
-            </h1>
+
+          <div class="relative z-10 flex flex-col items-center gap-2">
+            <p class="text-lg text-gray-200 text-center px-8">
+              Panel użytkownika - dostęp do podstawowych funkcji aplikacji
+            </p>
           </div>
-          <h2 class="text-xl">
-            Dostęp do podstawowych funkcji aplikacji
-          </h2>
         </div>
       </div>
     </div>
@@ -71,20 +59,17 @@
 definePageMeta({
   layout: 'none',
 })
-const router = useRouter()
 
+const router = useRouter()
 const adminBorderClass = ref('border-red-300 hover:border-red-500')
 const userBorderClass = ref('border-green-300 hover:border-green-500')
 
 const selectRole = (role: 'admin' | 'user') => {
-  console.log(`Wybrano rolę: ${role}`)
-
-  // Przekierowanie do odpowiedniego panelu
   if (role === 'admin') {
-    router.push('/admin')
+    router.push('/adminDashboard')
   }
   else {
-    router.push('/user')
+    router.push('/rooms')
   }
 }
 </script>
@@ -106,12 +91,14 @@ const selectRole = (role: 'admin' | 'user') => {
 .glow-text-red {
   text-shadow:
     0 0 5px #b91c1c,
-    0 0 10px #b91c1c;
+    0 0 10px #b91c1c,
+    0 0 20px #b91c1c;
 }
 
 .glow-text-green {
   text-shadow:
     0 0 5px #15803d,
-    0 0 10px #15803d;
+    0 0 10px #15803d,
+    0 0 20px #15803d;
 }
 </style>
